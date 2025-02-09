c397030f6522aba0c7125c12c91e720eb65c0887
Víctor Villena <knbosh@gmail.com>
2025-02-09 01:27:18 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,10 @@
+Global / onChangedBuildSource := ReloadOnSourceChanges
+
+ThisBuild / version      := "0.1.0-SNAPSHOT"
+ThisBuild / scalaVersion := "3.6.3"
+
+lazy val root = project
+  .in(file("."))
+  .settings(
+    name := "slox"
+  )
---
+++ src/main/scala/org/willena/slox/Main.scala
@@ -0,0 +1,13 @@
+package org.willena.slox
+
+import java.nio.charset.Charset
+import java.nio.file.{Files, Paths}
+
+object Main:
+
+  @main def run(path: String) =
+    val bytes = Files.readAllBytes(Paths.get(path))
+    exec(String(bytes, Charset.defaultCharset()))
+
+  private def exec(source: String) = ???
+    
